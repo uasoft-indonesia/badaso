adeb573ca3974848d3c189d0840658580a8b794c
Sulfano Agus Fikri <sulvanoagus.master@gmail.com>
2021-01-11 17:02:20 +0700

    add 1 seeder to seed all data

---
+++ src/Seeder/BadasoSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BadasoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(PermissionsSeeder::class);
+        $this->call(RolesSeeder::class);
+        $this->call(RolePermissionsSeeder::class);
+        $this->call(MenusSeeder::class);
+        $this->call(FixedMenuItemSeeder::class);
+        $this->call(SiteManagementSeeder::class);
+    }
+}
