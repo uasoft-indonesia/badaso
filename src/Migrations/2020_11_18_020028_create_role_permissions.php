7a4a7754f5a9c397c15c919753c12825acc2d5f5
Danny Atthaya <dannyatthaya9@gmail.com>
2021-01-21 14:27:14 +0700

    [ADD] add id on multiple table with logging

--- src/Migrations/2020_11_18_020028_create_role_permissions.php
+++ src/Migrations/2020_11_18_020028_create_role_permissions.php
@@ -15,12 +15,12 @@ class CreateRolePermissions extends Migration
     {
         try {
             Schema::create('role_permissions', function (Blueprint $table) {
+                $table->increments('id');
                 $table->unsignedInteger('role_id')->unsigned();
                 $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
                 $table->unsignedInteger('permission_id')->unsigned();
                 $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
                 $table->timestamps();
-                $table->primary(['role_id', 'permission_id']);
             });
         } catch (PDOException $ex) {
             $this->down();
--- src/Models/UserRole.php
+++ src/Models/UserRole.php
@@ -3,9 +3,12 @@
 namespace Uasoft\Badaso\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class UserRole extends Model
 {
+    use LogsActivity;
+
     protected $fillable = [
         'user_id',
         'role_id',
@@ -19,5 +22,14 @@ class UserRole extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected static $logAttributes = true;
+    protected static $logFillable = true;
+    protected static $logName = 'UserRole';
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "This model has been {$eventName}";
     }
 }
