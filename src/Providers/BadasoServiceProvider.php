1c34a78e0f01f311fb4b47f8f5e4538e47717da1
Sulfano agus fikri <sulvanoagus.master@gmail.com>
2021-02-16 08:49:04 +0700

    create separate publisher'

--- src/Providers/BadasoServiceProvider.php
+++ src/Providers/BadasoServiceProvider.php
@@ -44,6 +44,23 @@ class BadasoServiceProvider extends ServiceProvider
             __DIR__.'/../Images/' => public_path(),
             __DIR__.'/../resources/lang' => resource_path('lang/vendor/badaso'),
         ], 'Badaso');
+
+        $this->publishes([
+            __DIR__.'/../Config/badaso.php' => config_path('badaso.php'),
+            __DIR__.'/../Config/log-viewer.php' => config_path('log-viewer.php'),
+            __DIR__.'/../Config/backup.php' => config_path('backup.php'),
+        ], 'BadasoConfig');
+
+        $this->publishes([
+            __DIR__.'/../Seeder/Configurations' => database_path('seeds'),
+            __DIR__.'/../Seeder/CRUDData' => database_path('seeds/CRUDData'),
+        ], 'BadasoSeeder');
+
+        $this->publishes([
+            __DIR__.'/../resources/js/' => resource_path('js/badaso'),
+            __DIR__.'/../Images/' => public_path(),
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/badaso'),
+        ], 'BadasoResource');
     }
 
     /**
