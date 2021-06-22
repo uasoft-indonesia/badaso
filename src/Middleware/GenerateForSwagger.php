973bad7fda57550ba3aa4258c0743b85a23b4b81
my4fdal <my4fdal@gmail.com>
2021-06-22 16:38:39 +0700

    replace code typo

--- src/Middleware/GenerateForSwagger.php
+++ src/Middleware/GenerateForSwagger.php
@@ -3,8 +3,8 @@
 namespace Uasoft\Badaso\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Artisan;
 use Uasoft\Badaso\Helpers\ApiDocs;
-use Uasoft\Badaso\Helpers\Swagger\BadasoCRUDGeneratorGenerateSwagger;
 use Uasoft\Badaso\Models\DataType;
 
 class GenerateForSwagger
@@ -13,17 +13,27 @@ class GenerateForSwagger
     {
 
         switch ($request->getPathInfo()) {
-            case '/api-docs':
+            case $this->getPath(env('MIX_API_DOCUMENTATION_ROUTE', '/api-docs')):
                 $data_types = DataType::all();
-                foreach ($data_types as $key => $data_type) {
+                foreach ($data_types as $index => $data_type) {
                     $data_rows = $data_type->dataRows;
                     $table_name = $data_type->name;
 
                     ApiDocs::generateAPIDocs($table_name, $data_rows, $data_type);
                 }
+
+                Artisan::call("l5-swagger:generate");
                 break;
         }
 
         return $next($request);
+    }
+
+    public function getPath($api_docs)
+    {
+        if (substr($api_docs, 0, 1) != '/') {
+            $api_docs = "/{$api_docs}";
+        }
+        return $api_docs;
     }
 }
