fd7bfde963fbd3a6a59fb20fbb7fe6499a140c25
my4fdal <my4fdal@gmail.com>
2021-08-03 12:07:54 +0700

    no lincense key

--- src/Middleware/VerifyLicense.php
+++ src/Middleware/VerifyLicense.php
@@ -13,32 +13,36 @@ class VerifyLicense
 {
     public function handle($request, Closure $next)
     {
-        $license = env('BADASO_LICENSE_KEY');
-        if (is_null($license)) {
-            return ApiResponse::paymentRequired('BADASO_LICENSE_KEY not found');
-        } else {
-            // Call Badaso Dashboard API here
-            try {
-                $client = new Client();
-                $req = $client->request('POST', Badaso::getBadasoVerifyApi(), [
-                    'json' => [
-                        'license' => $license,
-                    ],
-                ]);
-            } catch (BadResponseException $e) {
-                if ($e->hasResponse()) {
-                    $response = $e->getResponse()->getBody();
-                    $status = $e->getResponse()->getStatusCode();
+        // not verify lincense
+        return ApiResponse::success([]);
 
-                    return response(json_decode($response, true), $status);
-                } else {
-                    return ApiResponse::failed($e);
-                }
-            } catch (Exception $e) {
-                return ApiResponse::failed($e);
-            }
-        }
+        // remove check verify lincense
+        // $license = env('BADASO_LICENSE_KEY');
+        // if (is_null($license)) {
+        //     return ApiResponse::paymentRequired('BADASO_LICENSE_KEY not found');
+        // } else {
+        //     // Call Badaso Dashboard API here
+        //     try {
+        //         $client = new Client();
+        //         $req = $client->request('POST', Badaso::getBadasoVerifyApi(), [
+        //             'json' => [
+        //                 'license' => $license,
+        //             ],
+        //         ]);
+        //     } catch (BadResponseException $e) {
+        //         if ($e->hasResponse()) {
+        //             $response = $e->getResponse()->getBody();
+        //             $status = $e->getResponse()->getStatusCode();
 
-        return $next($request);
+        //             return response(json_decode($response, true), $status);
+        //         } else {
+        //             return ApiResponse::failed($e);
+        //         }
+        //     } catch (Exception $e) {
+        //         return ApiResponse::failed($e);
+        //     }
+        // }
+
+        // return $next($request);
     }
 }
