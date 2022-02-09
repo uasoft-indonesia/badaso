05b6c1286565481ca82d21044c011a27dab2bf53
Rahmat <rahmatalmubarak35@gmail.com>
2022-02-09 08:33:47 +0700

    add function for callhelpertest

--- src/Helpers/CallHelperTest.php
+++ src/Helpers/CallHelperTest.php
@@ -118,4 +118,26 @@ class CallHelperTest
     {
         Cache::store('file')->clear();
     }
+
+    public static function login(TestCase $test_case)
+    {
+
+        $login = $test_case->post(CallHelperTest::getApiAuth('login'), [
+            "email" => "admin@admin.com",
+            "password" => "admin",
+            "remember" => false
+        ]);
+        return $login->json("data.accessToken");
+    }
+
+    public static function getApiAuth($path)
+    {
+        return "badaso-api/v1/auth/" . $path;
+    }
+
+    public static function getApiV1($path)
+    {
+        return "badaso-api/module/post/v1" . $path;
+    }
+
 }
