be668e355d1595d8112beb79dc8633b9b1e02563
my4fdal <my4fdal@gmail.com>
2022-03-08 16:23:40 +0700

    update migration

---
+++ src/Migrations/2022_03_08_090850_change_order_integer_data_type_in_menus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeOrderIntegerDataTypeInMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            if (!Schema::hasColumn(config('badaso.database.prefix') . 'menus', 'order')) {
+                $table->integer('order')->change();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            if (!Schema::hasColumn(config('badaso.database.prefix') . 'menus', 'order')) {
+                $table->integer('order')->change();
+            }
+        });
+    }
+}
