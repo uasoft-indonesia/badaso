9b9f66babfc97ee0757eab80e6621db1bbfc7635
Rizki Heryandi <rizkiheryandi@gmail.com>
2022-03-21 13:42:33 +0700

    update unit test and crud management

--- tests/Feature/BadasoApiCrudManagementTest.php
+++ tests/Feature/BadasoApiCrudManagementTest.php
@@ -222,7 +222,10 @@ class BadasoApiCrudManagementTest extends TestCase
                     'badaso_type' => 'upload_image_multiple',
                     'schema_type' => 'string',
                     'details' => json_encode((object) []),
-                    'example' => "['https://badaso-web.s3-ap-southeast-1.amazonaws.com/files/shares/1619582634819_badaso.png','https://badaso-web.s3-ap-southeast-1.amazonaws.com/files/shares/1619582634819_badaso.png']",
+                    'example' => [
+                        'https://badaso-web.s3-ap-southeast-1.amazonaws.com/files/shares/1619582634819_badaso.png',
+                        'https://badaso-web.s3-ap-southeast-1.amazonaws.com/files/shares/1619582634819_badaso.png',
+                    ],
                     'example_update' => [
                         'https://badaso-web.s3-ap-southeast-1.amazonaws.com/files/shares/1619581504968_uasoft.png',
                         'https://badaso-web.s3-ap-southeast-1.amazonaws.com/files/shares/1619581504968_uasoft.png',
@@ -531,7 +534,7 @@ class BadasoApiCrudManagementTest extends TestCase
             $request_body = $data_table_crud_management_log['request_body'];
             $table = $request_body['name'];
 
-             $response = CallHelperTest::withAuthorizeBearer($this)->json('GET', CallHelperTest::getUrlApiV1Prefix('/crud/read'), [
+            $response = CallHelperTest::withAuthorizeBearer($this)->json('GET', CallHelperTest::getUrlApiV1Prefix('/crud/read'), [
                 'table' => $table,
             ]);
             $response->assertSuccessful();
@@ -674,7 +677,7 @@ class BadasoApiCrudManagementTest extends TestCase
     {
         $data_add_entities = CallHelperTest::getCache($this->KEY_DATA_ADD_ENTITY);
         foreach ($data_add_entities as $table => $data_add_entity) {
-            $response = CallHelperTest::withAuthorizeBearer($this)->json('GET', CallHelperTest::getUrlApiV1Prefix("/entities/{$table}/all"));  
+            $response = CallHelperTest::withAuthorizeBearer($this)->json('GET', CallHelperTest::getUrlApiV1Prefix("/entities/{$table}/all"));
             $response->assertSuccessful();
         }
     }
