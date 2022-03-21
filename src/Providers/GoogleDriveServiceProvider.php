ef3ec87b71a80abf12af9dd38c8855477ebb03fd
my4fdal <my4fdal@gmail.com>
2022-03-21 09:51:05 +0700

    update composer.js

--- src/Providers/GoogleDriveServiceProvider.php
+++ src/Providers/GoogleDriveServiceProvider.php
@@ -19,8 +19,8 @@ class GoogleDriveServiceProvider extends ServiceProvider
             $client->setClientId($config['clientId']);
             $client->setClientSecret($config['clientSecret']);
             $client->refreshToken($config['refreshToken']);
-            $service = new \Google_Service_Drive($client);
-            $adapter = new \Hypweb\Flysystem\GoogleDrive\GoogleDriveAdapter($service, $config['folderId']);
+            $service = new \Google\Service\Drive($client);
+            $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service, $config['folderId']);
 
             return new \League\Flysystem\Filesystem($adapter);
         });
