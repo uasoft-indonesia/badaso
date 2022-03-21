7128af0bb4882ce7c7aba003a457c08d0cac35ed
my4fdal <my4fdal@gmail.com>
2022-03-21 11:59:04 +0700

    update badaso v3 support laravel 9

--- src/Models/MenuItem.php
+++ src/Models/MenuItem.php
@@ -3,6 +3,7 @@
 namespace Uasoft\Badaso\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 class MenuItem extends Model
@@ -70,5 +71,10 @@ class MenuItem extends Model
     public function getDescriptionForEvent(string $eventName): string
     {
         return "This model has been {$eventName}";
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults();
     }
 }
