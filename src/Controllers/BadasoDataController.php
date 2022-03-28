626275f36bd0efbd187b2c32c39c832a717ef542
Rizki Heryandi <rizkiheryandi@gmail.com>
2022-03-28 10:56:51 +0700

    change path controller

--- src/Controllers/BadasoDataController.php
+++ src/Controllers/BadasoDataController.php
@@ -2,7 +2,7 @@
 
 namespace Uasoft\Badaso\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Uasoft\Badaso\Facades\Badaso;
 use Uasoft\Badaso\Helpers\ApiResponse;
