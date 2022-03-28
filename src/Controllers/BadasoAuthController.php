9ac6b10b561dd1037e691bc74f06d4776adf5e9d
Rizki Heryandi <rizkiheryandi@gmail.com>
2022-03-28 13:41:27 +0700

    change controller

--- src/Controllers/BadasoAuthController.php
+++ src/Controllers/BadasoAuthController.php
@@ -2,7 +2,7 @@
 
 namespace Uasoft\Badaso\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
