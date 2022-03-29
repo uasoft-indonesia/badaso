b4a03226d2d0d52273f2252b3cac66f1939b04d4
4fdal <4fdal@users.noreply.github.com>
2022-03-29 03:25:29 +0000

    Apply eslit and prettier changes

--- src/resources/badaso/app.js
+++ src/resources/badaso/app.js
@@ -33,13 +33,13 @@ Vue.component("datetime", Datetime);
 Vue.use(Vuelidate);
 
 // IDENTIFIED VARIABLE BROADCAST CHANNEL
-                                          const broadcastChannelName = "sw-badaso-messages";
-                                          let broadcastChannel = null;
-                                          try {
-                                            broadcastChannel = new BroadcastChannel(broadcastChannelName);
-                                          } catch (error) {
-                                            console.error("Broadcast Channel Error ", error);
-                                          }
+const broadcastChannelName = "sw-badaso-messages";
+let broadcastChannel = null;
+try {
+  broadcastChannel = new BroadcastChannel(broadcastChannelName);
+} catch (error) {
+  console.error("Broadcast Channel Error ", error);
+}
 // END IDENTIFIED VARIABLE BROADCAST CHANNEL
 
 const pluginsEnv = process.env.MIX_BADASO_MODULES
