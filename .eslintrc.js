cdedc841db610b5b6c4c3eb63b0d9f0747b4356b
my4fdal <my4fdal@gmail.com>
2022-03-29 13:06:40 +0700

    gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "standard",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["vue", "flowtype", "prettier"],
+  globals: {
+    __WEEX__: true,
+    WXEnvironment: true,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV !== "production" ? 0 : 2,
+    "no-useless-escape": 0,
+    "no-empty": 0,
+    "vue/no-mutating-props": "off",
+    "no-prototype-builtins": "off",
+    eqeqeq: "off",
+  },
+};
