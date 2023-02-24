3a09369caa963a6f48e70eea143bdb247c04aa60
miftahurrahmi <miftahurrahmi45@gmail.com>
2023-02-24 09:51:23 +0700

    add widget component

--- src/resources/badaso/pages/home.vue
+++ src/resources/badaso/pages/home.vue
@@ -1,67 +1,6 @@
 <template>
   <vs-row>
-    <vs-col
-      v-for="(data, index) in dashboardData"
-      :key="index"
-      :vs-lg="col"
-      vs-xs="12"
-    >
-      <vs-card class="widget__content">
-        <div class="widget__icon-container">
-          <vs-icon
-            v-if="data.icon"
-            class="widget__icon"
-            :icon="data.icon"
-          ></vs-icon>
-          <template v-if="data.delimiter == ','">
-            <vs-row v-if="!data.prefixValue">
-              <h4 class="mb-1">
-                {{ setDelimiterComa(data.value) }}
-              </h4>
-            </vs-row>
-            <vs-row v-else>
-              <h4>{{ data.prefixValue }}</h4>
-              <h4 class="mb-1">
-                {{ setDelimiterComa(data.value) }}
-              </h4>
-            </vs-row>
-          </template>
-
-          <template v-else-if="data.delimiter == '.'">
-            <vs-row v-if="!data.prefixValue">
-              <h4 class="mb-1">
-                {{ setDelimiterPoint(data.value) }}
-              </h4>
-            </vs-row>
-            <vs-row v-else>
-              <h4>{{ data.prefixValue }}</h4>
-              <h4 class="mb-1">
-                {{ setDelimiterPoint(data.value) }}
-              </h4>
-            </vs-row>
-          </template>
-
-          <template v-else>
-            <vs-row v-if="!data.prefixValue">
-              <h4 class="mb-1">{{ data.value }}</h4>
-            </vs-row>
-            <vs-row v-else>
-              <h4>{{ data.prefixValue }}</h4>
-              <h4 class="mb-1">
-                {{ data.value }}
-              </h4>
-            </vs-row>
-          </template>
-          <span>{{ data.label }}</span>
-        </div>
-        <vs-progress
-          class="widget__progress-bar"
-          :percent="getPercent(data.value, data.max)"
-          :color="getProgressBarColor(data.value, data.max)"
-          >primary</vs-progress
-        >
-      </vs-card>
-    </vs-col>
+    <badaso-widget :col="col" :widgets="dashboardData"> </badaso-widget>
   </vs-row>
 </template>
 
@@ -129,23 +68,6 @@ export default {
           }
         });
       }
-    },
-    setDelimiterComa(price) {
-      const pieces = parseFloat(price).toFixed(2).split("");
-      let ii = pieces.length - 3;
-      while ((ii -= 3) > 0) {
-        pieces.splice(ii, 0, ",");
-      }
-      return pieces.join("").replace(/\.00/g, "");
-    },
-
-    setDelimiterPoint(price) {
-      const pieces = parseFloat(price).toFixed(2).split("");
-      let ii = pieces.length - 3;
-      while ((ii -= 3) > 0) {
-        pieces.splice(ii, 0, ",");
-      }
-      return pieces.join("").replace(/\.00/g, "").replace(/,/g, ".");
     },
   },
 };
