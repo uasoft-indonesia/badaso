530319861380fa5b672dcd7d191f90e2e54c48f0
miftahurrahmi <miftahurrahmi45@gmail.com>
2023-05-11 11:53:49 +0700

    commit

--- src/Config/backup.php
+++ src/Config/backup.php
@@ -101,7 +101,7 @@ return [
             \Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class         => ['mail'],
             \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => ['mail'],
             \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class        => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfuNotificationl::class     => ['mail'],
+            \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class     => ['mail'],
             \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class   => ['mail'],
             \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class    => ['mail'],
         ],
