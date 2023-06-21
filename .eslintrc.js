b1900da2e81e47ccef30dc3b1ba21176b33ee204
miftahurrahmi <miftahurrahmi45@gmail.com>
2023-06-21 14:17:55 +0700

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:prettier/recommended",
   ],
-  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
