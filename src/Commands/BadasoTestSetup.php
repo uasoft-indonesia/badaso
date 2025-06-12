b4cfbb4ac935c1a0a53a0bb1dfe49a211dfb3d07
TaufiqTzy <taufiq@example.com>
2025-06-12 14:31:33 +0700

    resolve phpunit error

--- src/Commands/BadasoTestSetup.php
+++ src/Commands/BadasoTestSetup.php
@@ -42,7 +42,16 @@ class BadasoTestSetup extends Command
 
         $phpunit_xml_content = <<<'XML'
         <?xml version="1.0" encoding="UTF-8"?>
-        <phpunit xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="./vendor/phpunit/phpunit/phpunit.xsd" bootstrap="vendor/autoload.php" colors="true" displayDetailsOnTestsThatTriggerWarnings="true" cacheDirectory=".phpunit.cache">
+        <phpunit xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+                 xsi:noNamespaceSchemaLocation="./vendor/phpunit/phpunit/phpunit.xsd" 
+                 bootstrap="vendor/autoload.php" 
+                 colors="true"
+                 executionOrder="depends,defects"
+                 failOnRisky="true"
+                 failOnWarning="true"
+                 failOnEmptyTestSuite="true"
+                 beStrictAboutOutputDuringTests="true"
+                 verbose="true">
             <testsuites>
                 <testsuite name="Unit">
                     <directory suffix="Test.php">./tests/Unit</directory>
@@ -55,6 +64,7 @@ class BadasoTestSetup extends Command
             </testsuites>
             <source>
                 <include>
+                    <!-- Uncomment line below to include app directory -->
                     <!-- <directory suffix=".php">./app</directory> -->
                     <directory suffix=".php">./vendor/badaso/core/src/Commands</directory>
                     <directory suffix=".php">./vendor/badaso/core/src/Controllers</directory>
@@ -71,15 +81,26 @@ class BadasoTestSetup extends Command
                     <directory suffix=".php">./vendor/badaso/core/src/Routes</directory>
                     <directory suffix=".php">./vendor/badaso/core/src/Traits</directory>
                     <directory suffix=".php">./vendor/badaso/core/src/Widgets</directory>
-                    <directory suffix=".php">./vendor/badaso/core/src/Badaso.php</directory>
+                    <file>./vendor/badaso/core/src/Badaso.php</file>
                 </include>
+                <exclude>
+                    <directory>./vendor/badaso/core/tests</directory>
+                    <directory>./tests</directory>
+                </exclude>
             </source>
+            <coverage>
+                <report>
+                    <clover outputFile="clover.xml"/>
+                    <html outputDirectory="coverage-html" lowUpperBound="50" highLowerBound="80"/>
+                    <text outputFile="coverage.txt" showUncoveredFiles="false" showOnlySummary="true"/>
+                </report>
+            </coverage>
             <php>
                 <server name="APP_ENV" value="testing"/>
                 <server name="BCRYPT_ROUNDS" value="4"/>
                 <server name="CACHE_DRIVER" value="array"/>
-                <server name="DB_CONNECTION" value="sqlite"/>
-                <server name="DB_DATABASE" value=":memory:"/>
+                <!-- <server name="DB_CONNECTION" value="sqlite"/> -->
+                <!-- <server name="DB_DATABASE" value=":memory:"/> -->
                 <server name="MAIL_MAILER" value="array"/>
                 <server name="QUEUE_CONNECTION" value="sync"/>
                 <server name="SESSION_DRIVER" value="array"/>
@@ -89,5 +110,14 @@ class BadasoTestSetup extends Command
         XML;
 
         file_put_contents($phpunit_xml_path, $phpunit_xml_content);
+        
+        $this->info('PHPUnit configuration file has been created successfully!');
+        $this->line('');
+        $this->line('To run tests with coverage, use one of these commands:');
+        $this->line('  ./vendor/bin/phpunit --coverage-html coverage-html');
+        $this->line('  ./vendor/bin/phpunit --coverage-clover clover.xml');
+        $this->line('  ./vendor/bin/phpunit --coverage-text');
+        
+        return 0;
     }
 }
